e864cf0bdb779da92ac61a8b2d2c3d584e89755c
Rain <niarmeowgrin@gmail.com>
2026-05-03 00:35:45 +0200

    add onMouseClick event to InventorySideGui.kt add /gfs feature to PestBaitType.kt

--- src/main/kotlin/me/owdding/skyocean/features/garden/PestBaitType.kt
+++ src/main/kotlin/me/owdding/skyocean/features/garden/PestBaitType.kt
@@ -1,17 +1,15 @@
 package me.owdding.skyocean.features.garden
 
-import earth.terrarium.olympus.client.components.buttons.Button
 import me.owdding.ktmodules.Module
 import me.owdding.lib.builder.LayoutFactory
 import me.owdding.lib.builder.MIDDLE
-import me.owdding.lib.displays.DisplayWidget
 import me.owdding.lib.displays.Displays
-import me.owdding.lib.displays.withPadding
 import me.owdding.skyocean.config.features.garden.GardenConfig
 import me.owdding.skyocean.helpers.InventorySideGui
 import me.owdding.skyocean.utils.Utils.unaryMinus
 import me.owdding.skyocean.utils.chat.ChatUtils
 import net.minecraft.client.gui.layouts.Layout
+import net.minecraft.client.gui.layouts.LayoutElement
 import tech.thatgravyboat.skyblockapi.api.area.farming.garden.pests.Pest
 import tech.thatgravyboat.skyblockapi.api.area.farming.garden.pests.Spray
 import tech.thatgravyboat.skyblockapi.api.events.screen.ContainerInitializedEvent
@@ -41,13 +39,19 @@ object PestBaitType : InventorySideGui("(?:Pest|Mouse|Vermin) Trap", { GardenCon
                 }
             }
         }
-
-        val buttonDisplay = Displays.component(-"garden.open_sack").withPadding(2)
-        val sacksButton = Button().apply {
-            withRenderer(DisplayWidget.displayRenderer(buttonDisplay))
-            setSize(buttonDisplay.getWidth(), buttonDisplay.getHeight())
-            withCallback { McClient.sendCommand("/sacks") }
+    }.also { layout ->
+        val sprayRows = mutableListOf<LayoutElement>()
+        var skippedHeader = false
+        layout.visitChildren {
+            if (!skippedHeader) { skippedHeader = true; return@visitChildren }
+            sprayRows.add(it)
         }
-        widget(sacksButton)
+
+        Spray.entries.forEachIndexed { index, spray ->
+            val row = sprayRows.getOrNull(index) ?: return@forEachIndexed
+            registerClickRegion(row.x, row.y, row.width, row.height) {
+                McClient.sendCommand("gfs ${spray.name} 64")
+            }
+        }
     }
 }
--- src/main/kotlin/me/owdding/skyocean/helpers/InventorySideGui.kt
+++ src/main/kotlin/me/owdding/skyocean/helpers/InventorySideGui.kt
@@ -12,6 +12,7 @@ import org.intellij.lang.annotations.Language
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.screen.ContainerCloseEvent
 import tech.thatgravyboat.skyblockapi.api.events.screen.ContainerInitializedEvent
+import tech.thatgravyboat.skyblockapi.api.events.screen.ScreenMouseClickEvent
 import tech.thatgravyboat.skyblockapi.api.events.screen.ScreenInitializedEvent
 import tech.thatgravyboat.skyblockapi.utils.extentions.left
 import tech.thatgravyboat.skyblockapi.utils.extentions.right
@@ -28,9 +29,16 @@ abstract class InventorySideGui(@Language("RegExp") titleRegex: String, val alig
     private var lastEvent: ContainerInitializedEvent? = null
     private var isBeingShown: Boolean = false
 
+    private data class ClickRegion(val x: Int, val y: Int, val width: Int, val height: Int, val onClick: () -> Unit)
+    private val clickRegions = mutableListOf<ClickRegion>()
+
     abstract val enabled: Boolean
 
     protected abstract fun ContainerInitializedEvent.getLayout(): Layout?
+
+    protected fun registerClickRegion(x: Int, y: Int, width: Int, height: Int, onClick: () -> Unit) {
+        clickRegions.add(ClickRegion(x, y, width, height, onClick))
+    }
 
     fun refresh() {
         val event = lastEvent ?: return
@@ -38,6 +46,7 @@ abstract class InventorySideGui(@Language("RegExp") titleRegex: String, val alig
 
         isBeingShown = enabled && regex.matches(screen.title.stripped)
         if (!isBeingShown) return
+        clickRegions.clear()
 
         val layout = event.getLayout() ?: return
 
@@ -68,6 +77,19 @@ abstract class InventorySideGui(@Language("RegExp") titleRegex: String, val alig
         val widget = this.oldWidget ?: return
 
         event.screen.addWidget(widget)
+    }
+
+    @Subscription(inherited = true)
+    fun onMouseClicked(event: ScreenMouseClickEvent) {
+        if (!isBeingShown) return
+        val widget = oldWidget ?: return
+        val relX = event.x - widget.x
+        val relY = event.y - widget.y
+        clickRegions.forEach { (x, y, width, height, onClick) ->
+            if (relX >= x && relX <= x + width && relY >= y && relY <= y + height) {
+                onClick()
+            }
+        }
     }
 
     private fun Screen.addWidget(widget: AbstractWidget) {
